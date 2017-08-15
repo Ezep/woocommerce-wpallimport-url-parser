b1aea4c63868d1f6341135b0833c1219eb601f41
Ezequiel Morfu <ezequiel.morfu@gmail.com>
2017-08-15 02:41:02 -0300

    Create test.php

---
+++ wp-content/uploads/wpallimport/test.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once('functions.php');
+
+$url = http://www.somewoostore.com/products/product-123/
+
+var_dump(name('', $url));
+var_dump(desc('', $url));
+var_dump(short_desc('', $url));
+var_dump(image('', $url));
